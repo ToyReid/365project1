4f56a986b2489095f7407bbc16adf2ec99340f13
Chris Dean <cdean16@vols.utk.edu>
2018-03-20 16:57:29 -0400

    Added gitignore and debug in Firstpass.cs

--- FirstPass.cs
+++ FirstPass.cs
@@ -1,9 +1,12 @@
+using System;
+using System.IO;
 using System.Collections.Generic;
+
 
 class FirstPass
 {
 
-	public statice void Main(string[] args)
+	public static void Main(string[] argv)
 	{
 		var fp = new FirstPass(argv[0]);
 		fp.getCleanInput();
@@ -18,33 +21,28 @@ class FirstPass
 
 	public List<string> getCleanInput()
 	{
-		List<string> rv;
+		List<string> rv = new List<string>();
 		string line;
 
 		using(var sr = new StreamReader(mFileName))
 		{
-			int index;
-			while(sr.BaseStream.Position != sr.BaseStream.Length)
+
+			while((line = sr.ReadLine()) != null)
 			{
-				line = sr.ReadLine();
-
-				if(line.IsNullOrWhitespace()) continue;
-
-				StripComments(line, "#");
-				StripComments(line, "//");
+				line = StripComments(line, "#");
+				line = StripComments(line, "//");
 
 				Console.WriteLine(line);
 			}
 
 
 		}
-	}
+		return rv;
+	}//ladskfjsldkfj
 
 	public static string StripComments(string line, string commentDelimiter)
 	{
 		int index;
-
-		if(line.IsNullOrEmpty) return line;
 
 		index = line.IndexOf(commentDelimiter);
 
