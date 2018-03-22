e7dff264870934be0c87ba68c9ef72eeccbb3795
Chris Dean <chrisdean258@gmail.com>
2018-03-22 16:27:53 -0400

    added label to label.cs

--- Label.cs
+++ Label.cs
@@ -1,14 +1,22 @@
 public class Label : ILabel
 {
-    protected uint address;
+	protected uint address;
+	protected string label;
 
-    public uint Address {
-        get {
-            return address;
-        }
-    }
+	public uint Address {
+		get {
+			return address;
+		}
+	}
 
-    public Label(uint addr) {
-        address = addr;
-    }
+	public string Label{
+		get{
+			return label;
+		}
+	}
+
+	public Label(string lab, uint addr) {
+		address = addr;
+		label = lab;
+	}
 }
